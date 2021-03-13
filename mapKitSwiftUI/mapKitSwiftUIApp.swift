4ea9c968289a68e57462eac1bcfa8246adf08272
Mauro <mauro.filho@ua.pt>
2021-03-13 08:50:46 +0000

    Initial Commit

---
+++ mapKitSwiftUI/mapKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapKitSwiftUIApp.swift
+//  mapKitSwiftUI
+//
+//  Created by Mauro Canhao on 13/03/21.
+//
+
+import SwiftUI
+
+@main
+struct mapKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
